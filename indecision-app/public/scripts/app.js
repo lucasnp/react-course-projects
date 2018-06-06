4a0360ac9e1769bf56751f961d95b603fd8528b7
Nemo <lucasnp@gmail.com>
2018-06-05 21:53:24 -0500

    s3l10

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,54 @@ console.log('running');
 
 //JSX - JavaScript XML
 var template = React.createElement(
-  'p',
-  { id: 'my_id' },
-  'It changed!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'My app'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is the new content'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Nemo'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 30'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Houston, TX'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,25 @@
 console.log('running');
 
 //JSX - JavaScript XML
-var template = <p id='my_id'>It changed!</p>;
+var template = (
+    <div>
+        <h1>My app</h1>
+        <p>This is the new content</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Nemo</h1>
+        <p>Age: 30</p>
+        <p>Location: Houston, TX</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
